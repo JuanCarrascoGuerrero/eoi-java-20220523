9df1768ddad2c0af4c31313ebfcc30f82b7b4197
juanc <juancarrascoguerrero@gmail.com>
2022-05-23 17:34:10 +0200

    count_function branch created Counter.java created

--- Counter.java
+++ Counter.java
@@ -1,7 +1,9 @@
 public class Counter {
     public static void main(String[] args) {
-        for (int i = 0; i <=1000 ; i++) {
-            System.out.println(i);
+
+            for (int i = 0; i <= 1000; i++) {
+                System.out.println(i);
+            }
         }
-    }
+
 }
